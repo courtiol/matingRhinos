c1e9a4972a4820baf10bdcdf28f119c8963014be
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-07-15 09:40:28 +0200

    documentation for datasets

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' The datasets
+#'
+#' Those 4 datasets are those used in the paper. There are two datasets
+#' providing individual-level data (one for males, one for females) and two
+#' datasets providing data aggregated by 'mating success - reproductive success
+#' - male cohort' combination (also one for males and one for females). The
+#' aggregated data are used for ploting only.
+#' 
+#' @name males
+#' @aliases males females males_agg females_agg
+#' @docType data
+#' @format A \var{dataframe}.
+#' @references This study.
+#' @keywords datasets
+#' @examples
+#' males
+#' females
+#' males_agg
+#' females_agg
+NULL

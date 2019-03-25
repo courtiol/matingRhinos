184544306570ff6f6553067547a8a9cb56622ff0
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2019-03-25 15:01:21 +0100

    updates title

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,10 @@
   ## This function should not be called by the user.
   ## It displays a message when the package is being loaded.
   packageStartupMessage(## display message
-    '\n Welcome to adRes,',
+    '\n Welcome to matingRhinos,',
     '\n ',
     '\n This package has been created to document the analysis of the paper:',
-    '\n Mate choice, reproductive success and inbreeding in white rhinos',
+    '\n Mate choice, reproductive success and inbreeding in white rhinoceros...',
     '\n by Kretzschmar et al. (in prep)',
     '\n ',
     '\n Type ?matingRhinos for information about how to use this package!',

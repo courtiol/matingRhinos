d42f8b7ea0fefda351e75fe53c14628fc582b576
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2019-04-01 18:18:34 +0200

    Prep for Colin

--- R/not_yet_included/relatedness.R
+++ R/not_yet_included/relatedness.R
@@ -11,6 +11,8 @@ d$real <- rep(c(0.5, 0.5, 0.25, 1/8, 1/32, 0), each = 100)
 d$cat <- as.factor(rep(c("PO", "FS", "HS", "FC", "SC", "U"), each = 100))
 d$cat <- reorder(d$cat, rev(d$real))
 
+## COLIN: PUT ALL ABOVE IN RDA AND STEPS IN prepare_data.R
+
 res <- c(TriolML = sum((d$TrioMl - d$real)^2),
   Wang = sum((d$Wang - d$real)^2),
   LynchLi =  sum((d$LynchLi - d$real)^2),
@@ -34,9 +36,9 @@ sort(res2, decreasing = TRUE)
 boxplot(DyadML ~ cat, data = d)  ### COLIN: PLEASE MAKE THIS NICE
 abline(h = c(0.5, 0.5, 0.25, 1/8, 1/32, 0), lty = 3)
 
-wilcox.test(d$DyadML[d$cat == "PO"], mu = 0.5)
-wilcox.test(d$DyadML[d$cat == "FS"], mu = 0.5)
-wilcox.test(d$DyadML[d$cat == "HS"], mu = 0.25)
-wilcox.test(d$DyadML[d$cat == "FC"], mu = 1/8)
-wilcox.test(d$DyadML[d$cat == "SC"], mu = 1/32)
-wilcox.test(d$DyadML[d$cat == "U"], mu = 0)
+# wilcox.test(d$DyadML[d$cat == "PO"], mu = 0.5)
+# wilcox.test(d$DyadML[d$cat == "FS"], mu = 0.5)
+# wilcox.test(d$DyadML[d$cat == "HS"], mu = 0.25)
+# wilcox.test(d$DyadML[d$cat == "FC"], mu = 1/8)
+# wilcox.test(d$DyadML[d$cat == "SC"], mu = 1/32)
+# wilcox.test(d$DyadML[d$cat == "U"], mu = 0)
